9de9884e189483ab7f60538075e812af0f5464df
abdulaziz <115085054+Azoz42@users.noreply.github.com>
2023-12-11 23:31:43 +0300

    login

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
+if (process.env.NODE_Env !== 'production'){
+  require('dotenv').config()
+}
+
 const express = require('express')
 const path = require('path')
 const bcrypt = require('bcrypt')
 const app = express()
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+
+const initializPassport = require('./passport-config.js')
+initializPassport(
+  passport, 
+  email => users.find(user => user.email === email)
+  )
+
+const users = []
+
 
 // setup static and middleware
 app.use(express.static('./SWE363 Project/css'))
@@ -11,34 +27,48 @@ app.use(express.static('./SWE363 Project/images'))
 app.use(express.static('./SWE363 Project/html'))
 
 app.use(express.urlencoded({ extended: false }))
-
+app.set('view-engine', 'ejs')
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, './SWE363 Project/html/index.html'))
 })
 
-app.get('/', (req, res) => {
-  res.render('register.html')
+app.get('/register', (req, res) => {
+  res.render('register.ejs')
 })
 
-app.post('/register.html', async (req, res) => {
+app.post('/register', async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    URLSearchParams.push({
+    users.push({
       id: Date.now().toString(),
       name: req.body.name,
       email: req.body.email,
       password: hashedPassword
     })
-    res.redirect('/login.html')
+    res.redirect('/login')
   } catch {
-    res.redirect('/register.html')
+    res.redirect('/register')
   }
-  console.log
+  console.log(users)
 })
 
-app.post('/login.html', (req, res) => {
-  
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
+app.get('/login', (req, res) => {
+  res.render('login.ejs')
 })
 
 app.all('*', (req, res) => {
