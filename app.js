60e4f6c33c9fba2b777f13ea5c726e761716ce4e
Mcs-70 <112734754+Mcs-70@users.noreply.github.com>
2023-12-12 23:19:58 +0300

    solve the secret problem

--- app.js
+++ app.js
@@ -1,80 +1,81 @@
-if (process.env.NODE_Env !== 'production'){
-  require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
 }
 
-const express = require('express')
-const path = require('path')
-const bcrypt = require('bcrypt')
-const app = express()
-const passport = require('passport')
-const flash = require('express-flash')
-const session = require('express-session')
+const express = require('express');
+const path = require('path');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
 
-const initializPassport = require('./passport-config.js')
-initializPassport(
+const initializePassport = require('./passport-config');
+const users = []; // Users array should be defined before calling initializePassport
+
+initializePassport(
   passport, 
-  email => users.find(user => user.email === email)
-  )
+  email => users.find(user => user.email === email),
+);
 
-const users = []
+const app = express();
 
+// Static files middleware
+app.use(express.static('./SWE363 Project/css'));
+app.use(express.static('./SWE363 Project/fonts'));
+app.use(express.static('./SWE363 Project/js')); // Changed 'JS' to 'js' for consistency
+app.use(express.static('./SWE363 Project/images'));
+app.use(express.static('./SWE363 Project/html'));
 
-// setup static and middleware
-app.use(express.static('./SWE363 Project/css'))
-app.use(express.static('./SWE363 Project/fonts'))
-app.use(express.static('./SWE363 Project/JS'))
-app.use(express.static('./SWE363 Project/images'))
-app.use(express.static('./SWE363 Project/html'))
-
-app.use(express.urlencoded({ extended: false }))
-app.set('view-engine', 'ejs')
-app.use(flash())
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'ejs'); // Corrected 'view-engine' to 'view engine'
+app.use(flash());
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: process.env.SESSION_SECRET || 'secret',
   resave: false,
   saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
+// Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './SWE363 Project/html/index.html'))
-})
+  res.sendFile(path.resolve(__dirname, './SWE363 Project/html/index.html'));
+});
 
 app.get('/register', (req, res) => {
-  res.render('register.ejs')
-})
+  res.render('register.ejs');
+});
 
 app.post('/register', async (req, res) => {
   try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
     users.push({
       id: Date.now().toString(),
       name: req.body.name,
       email: req.body.email,
       password: hashedPassword
-    })
-    res.redirect('/login')
+    });
+    res.redirect('/login');
   } catch {
-    res.redirect('/register')
+    res.redirect('/register');
   }
-  console.log(users)
-})
+});
+
+app.get('/login', (req, res) => {
+  res.render('login.ejs');
+});
 
 app.post('/login', passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/login',
   failureFlash: true
-}))
-
-app.get('/login', (req, res) => {
-  res.render('login.ejs')
-})
+}));
 
 app.all('*', (req, res) => {
-  res.status(404).send('resource not found')
-})
+  res.status(404).send('Resource not found');
+});
 
 app.listen(3000, () => {
-  console.log('server is listening on port 3000....')
-})
+  console.log('Server is listening on port 3000...');
+});
