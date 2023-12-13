ad47d7349e7a394b5bec7e8ba87c69161beeeb9e
Waleed201 <aannsq23@gmail.com>
2023-12-14 01:06:17 +0300

    ..

--- app.js
+++ app.js
@@ -108,6 +108,10 @@ app.get('/covreges', (req,res) => {
   res.render('covreges')
 })
 
+app.get('/events', (req,res) => {
+  res.render('events')
+})
+
 app.all('*', (req, res) => {
   res.status(404).send('Resource not found');
 });
