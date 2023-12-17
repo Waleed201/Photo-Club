2534dc4c4d52684df88df6a0f4051becb1d80ad1
Mcs-70 <112734754+Mcs-70@users.noreply.github.com>
2023-12-18 02:31:09 +0300

    some changes

--- server.js
+++ server.js
@@ -338,6 +338,6 @@ app.all('*', (req, res) => {
 
 require('dotenv').config(); // Add this line to load environment variables from .env file
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is listening on port ${port}...`));
 module.exports = app;
